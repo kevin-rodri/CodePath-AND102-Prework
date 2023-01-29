30315d9d9a147907e1f99af01fa6f0468e3575bb
kevin-rodri <kr399880@gmail.com>
2023-01-28 21:00:51 -0500

    init

---
+++ app/src/main/java/edu/quinnipiac/ser210/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.quinnipiac.ser210.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Toast.makeText(this, "Nice to meet you!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
